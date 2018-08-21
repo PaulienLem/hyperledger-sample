6f931c5804781ffbbef5da18cfb8f0645342ef53
paulienlem <paulien.lemay@sweetmustard.be>
2018-08-21 20:58:50 +0200

    remove keystore on teardown

--- blockchain/teardown.sh
+++ blockchain/teardown.sh
@@ -3,7 +3,7 @@
 set -e
 docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
 
-rm -fr ./hfc-key-store
+rm -fr ../hyperledger-backend/app/services/hfc-key-store
 
 docker rm $(docker ps -aq)
 docker rmi $(docker images dev-* -q)
